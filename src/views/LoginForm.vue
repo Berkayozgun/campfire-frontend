5ba11c735956c129b5b375727fff703cb64bc76e
Berkayozgun <berkayozgun001@gmail.com>
2024-09-07 20:21:27 +0300

    Update styles in CreatePost.css and CustomHeader.css

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,107 +1,130 @@
 <template>
-    <div class="login-form">
-        <h1>Login</h1>
-        <form @submit.prevent="login">
-            <label for="username">Username:</label>
-            <input type="text" id="username" v-model="username" required>
-            <label for="password">Password:</label>
-            <input type="password" id="password" v-model="password" required>
-            <button type="submit">Login</button>
-        </form>
-    </div>
+  <div class="login-form">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="username">Username:</label>
+      <input class="name-input" type="text" id="username" v-model="username" required />
+      <label for="password">Password:</label>
+      <input class="pw-input" type="password" id="password" v-model="password" required />
+      <button class="submit-button" type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            username: '',
-            password: ''
-        };
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Login Failed");
+        }
+
+        const responseData = await response.json();
+        console.log("Response Data:", responseData);
+
+        console.clear();
+
+        if (!responseData.username || !responseData.access_token) {
+          throw new Error("Invalid server response format");
+        }
+
+        // Update the Vuex store with user information and authentication token
+        await this.$store.dispatch("setToken", responseData.access_token);
+        await this.$store.dispatch("setUser", {
+          username: responseData.username,
+          access_token: responseData.access_token,
+        });
+
+        // Log success message
+        console.log(
+          "Login Successful - Username:",
+          this.$store.state.user.username
+        );
+
+        // Redirect to the desired page
+        this.$router.push("/");
+      } catch (error) {
+        console.log("Login error:", error);
+      }
     },
-    methods: {
-        async login() {
-            try {
-                const response = await fetch('http://127.0.0.1:5000/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        username: this.username,
-                        password: this.password,
-                    }),
-                });
-
-                if (!response.ok) {
-                    throw new Error("Login Failed");
-                }
-
-                const responseData = await response.json();
-                console.log('Response Data:', responseData);
-
-                console.clear();
-
-                if (!responseData.username || !responseData.access_token) {
-                    throw new Error('Invalid server response format');
-                }
-
-                // Update the Vuex store with user information and authentication token
-                await this.$store.dispatch('setToken', responseData.access_token);
-                await this.$store.dispatch('setUser', { username: responseData.username, access_token: responseData.access_token });
-
-                // Log success message
-                console.log('Login Successful - Username:', this.$store.state.user.username);
-
-                // Redirect to the desired page
-                this.$router.push('/');
-            } catch (error) {
-                console.log("Login error:", error);
-            }
-        },
-
-    },
-
+  },
 };
 </script>
 
 <style scoped>
 .login-form {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+  max-width: 400px;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: Lato, sans-serif;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  background-color: #f9f9f9;
+  border-radius: 50px;
 }
 
 .login-form h1 {
-    text-align: center;
+  text-align: center;
 }
 
 .login-form form {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .login-form label {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 
 .login-form input {
-    padding: 10px;
-    margin-bottom: 20px;
+  padding: 10px;
+  margin-bottom: 20px;
 }
 
 .login-form button {
-    padding: 10px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  padding: 10px;
+  background-color: #007bff;
+  opacity: 0.8;
+  color: #fff;
+  border: none;
+  border-radius: 40px;
+  cursor: pointer;
 }
 
 .login-form button:hover {
-    background-color: #0056b3;
+  background-color: #0056b3;
+}
+
+.login-form .name-input {
+  text-transform: lowercase;
+  border-radius: 50px;
+  border:none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.login-form .pw-input {
+  font-family: monospace;
+  border-radius: 50px;
+  border:none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 }
 </style>
--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -3,22 +3,22 @@
     <h2>Register</h2>
     <form @submit.prevent="registerUser">
       <label for="username">Username:</label>
-      <input type="text" id="username" v-model="username" required>
+      <input type="text" id="username" v-model="username" required />
 
       <label for="email">Email:</label>
-      <input type="email" id="email" v-model="email" required>
+      <input type="email" id="email" v-model="email" required />
 
       <label for="password">Password:</label>
-      <input type="password" id="password" v-model="password" required>
+      <input type="password" id="password" v-model="password" required />
 
       <label for="name">Name:</label>
-      <input type="text" id="name" v-model="name" required>
+      <input type="text" id="name" v-model="name" required />
 
       <label for="surname">Surname:</label>
-      <input type="text" id="surname" v-model="surname" required>
+      <input type="text" id="surname" v-model="surname" required />
 
       <label for="birthdate">Birthdate:</label>
-      <input type="date" id="birthdate" v-model="birthdate" required>
+      <input type="date" id="birthdate" v-model="birthdate" required />
 
       <label for="gender">Gender:</label>
       <select id="gender" v-model="gender" required>
@@ -28,26 +28,31 @@
       </select>
 
       <label for="profileImageUrl">Profile Image URL:</label>
-      <input type="text" id="profile_image_url" v-model="profile_image_url" required>
+      <input
+        type="text"
+        id="profile_image_url"
+        v-model="profile_image_url"
+        required
+      />
 
       <button type="submit">Register</button>
     </form>
   </div>
 </template>
-  
+
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   data() {
     return {
-      username: '',
-      email: '',
-      password: '',
-      name: '',
-      surname: '',
-      birthdate: '',
-      gender: '',
-      profile_image_url: '',
+      username: "",
+      email: "",
+      password: "",
+      name: "",
+      surname: "",
+      birthdate: "",
+      gender: "",
+      profile_image_url: "",
     };
   },
   methods: {
@@ -63,10 +68,10 @@ export default {
         profile_image_url: this.profile_image_url,
       };
 
-      axios.post('http://127.0.0.1:5000/register', userData)
+      axios
+        .post("http://127.0.0.1:5000/register", userData)
         .then((response) => {
           console.log(response.data);
-
         })
         .catch((error) => {
           console.log(error);
@@ -75,14 +80,16 @@ export default {
   },
 };
 </script>
-  
+
 <style scoped>
 .register-form {
   max-width: 400px;
   margin: 0 auto;
+  margin-top: 10px;
   padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
+  border-radius: 35px;
+  font-family: Lato, sans-serif;
 }
 
 h2 {
@@ -95,19 +102,27 @@ label {
   margin-bottom: 10px;
 }
 
-input,
+input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border:none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+}
 select {
   width: 100%;
   padding: 10px;
   margin-bottom: 20px;
-  border: 1px solid #ccc;
+  border:none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
   border-radius: 4px;
 }
 
 button {
   width: 100%;
   padding: 10px;
-  background-color: #4CAF50;
+  background-color: #4caf50;
   color: white;
   border: none;
   border-radius: 4px;
@@ -118,4 +133,3 @@ button:hover {
   background-color: #45a049;
 }
 </style>
-  
